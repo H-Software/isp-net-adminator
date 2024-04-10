9935a127da15fe60aa6d48044605aec40773a588
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-10 22:41:12 +0200

    routing

--- adminator3/home-slim.php
+++ adminator3/home-slim.php
@@ -21,9 +21,24 @@ $app = new \Slim\App(['settings' => $slim_config]);
 
 require "app/src/dependencies.php";
 
-// app routing
+// routes
 use App\Middleware\GuestMiddleware;
 use App\Middleware\AuthMiddleware;
+
+$app->group('', function () {
+	$this->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
+	$this->post('/auth/signup', 'AuthController:postSignUp');
+	$this->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
+	$this->post('/auth/signin', 'AuthController:postSignIn');
+})->add(new GuestMiddleware($container));
+
+
+$app->group('', function () {
+    $this->get('/', 'HomeController:index')->setName('home');
+	$this->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
+	$this->get('/auth/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
+	$this->post('/auth/password/change', 'PasswordController:postChangePassword');
+})->add(new AuthMiddleware($container));
 
 $app->group('', function () {
     $this->map(['GET', 'POST'],'/home', \homeController::class . ':home');
