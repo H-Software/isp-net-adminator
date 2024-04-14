5c6c9a3834234f1d8d1324f3df2bd39b0ad64039
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-15 00:35:04 +0200

    f

--- adminator3/models/stb.php
+++ adminator3/models/stb.php
@@ -574,37 +574,37 @@ class stb
                 $rs .= $rs_s;
 
                 // TODO: improve showing data from form
-                    $rs .= "<br>
-                    STB Objekt byl přidán/upraven, zadané údaje:<br><br>
-                    <b>Popis objektu</b>: " . $data['popis'] . "<br>
-                    <b>IP adresa</b>: " . $data['ip'] . "<br>
-                    <b>MAC adresa</b>: " . $data['mac'] . "<br><br>
-                    
-                    <b>Puk</b>: " . $data['puk'] . "<br>
-                    <b>Číslo portu switche</b>: " . $data['$port_id'] . "<br>
-                    
-                    <b>Přípojný bod</b>: ";
-
-                    $vysledek3=$this->conn_mysql->query("select jmeno, id from nod_list WHERE id='".intval($data['id_nodu'])."' ");
-                    $radku3=$vysledek3->num_rows;
-                    
-                    if($radku3==0) $rs .= " Nelze zjistit ";
-                    else 
-                    {
-                        while( $zaznam3=$vysledek3->fetch_array() )
-                            { $rs .= $zaznam3["jmeno"]." (id: ".$zaznam3["id"].") ".''; }
-                    }
+                $rs .= "<br>
+                STB Objekt byl přidán/upraven, zadané údaje:<br><br>
+                <b>Popis objektu</b>: " . $data['popis'] . "<br>
+                <b>IP adresa</b>: " . $data['ip'] . "<br>
+                <b>MAC adresa</b>: " . $data['mac'] . "<br><br>
                 
-                    $rs .= "<br><br>";
-                    
-                    $rs .= "<b>Poznámka</b>:".htmlspecialchars($data['pozn'])."<br>";
-                    
-                    $ms_tarif = $this->conn_mysql->query("SELECT jmeno_tarifu FROM tarify_iptv WHERE id_tarifu = '".intval($data['id_tarifu'])."'");
-                    
-                    $ms_tarif->data_seek(0);
-                    $ms_tarif_r = $ms_tarif->fetch_row();
-                    
-                    $rs .= "<b>Tarif</b>: ".$ms_tarif_r[0]."<br><br>";
+                <b>Puk</b>: " . $data['puk'] . "<br>
+                <b>Číslo portu switche</b>: " . $data['$port_id'] . "<br>
+                
+                <b>Přípojný bod</b>: ";
+
+                $vysledek3=$this->conn_mysql->query("select jmeno, id from nod_list WHERE id='".intval($data['id_nodu'])."' ");
+                $radku3=$vysledek3->num_rows;
+                
+                if($radku3==0) $rs .= " Nelze zjistit ";
+                else 
+                {
+                    while( $zaznam3=$vysledek3->fetch_array() )
+                        { $rs .= $zaznam3["jmeno"]." (id: ".$zaznam3["id"].") ".''; }
+                }
+            
+                $rs .= "<br><br>";
+                
+                $rs .= "<b>Poznámka</b>:".htmlspecialchars($data['pozn'])."<br>";
+                
+                $ms_tarif = $this->conn_mysql->query("SELECT jmeno_tarifu FROM tarify_iptv WHERE id_tarifu = '".intval($data['id_tarifu'])."'");
+                
+                $ms_tarif->data_seek(0);
+                $ms_tarif_r = $ms_tarif->fetch_row();
+                
+                $rs .= "<b>Tarif</b>: ".$ms_tarif_r[0]."<br><br>";
 
                 $ret[0] = $rs;
                 return $ret;
