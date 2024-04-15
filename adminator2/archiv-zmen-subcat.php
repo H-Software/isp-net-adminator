204bc1141dfa4756ac2a8884575b26d0ea584b5d
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-15 18:34:23 +0200

    f

--- adminator2/archiv-zmen-subcat.php
+++ adminator2/archiv-zmen-subcat.php
@@ -1,11 +1,7 @@
 <span style="margin-left: 40px; "><a href="home.php" class="odkaz-uroven-vys" >| O úrověn výš |</a></span>
 
-<span style="padding-left: 30px; "><!--<a href="archiv-zmen.php">-->Archiv změn<!--</a>--></span>
+<span style="padding-left: 30px; "><!--<a href="archiv-zmen.php">-->Archiv změn (a3)<!--</a>--></span>
 
 <span style="padding-left: 30px; "><!--<a href="archiv-zmen-work.php">-->Archiv - Work (restartování) (a3)<!--</a>--></span>
 
-<span style="padding-left: 30px; ">
-    <span style="padding-right: 10px; ">A3:</span>  
-    <a href=
-<?php echo fix_link_to_another_adminator("/archiv-zmen/ucetni"); ?>
-    >Změny pro účetní</a></span>
+<span style="padding-left: 30px; ">Změny pro účetní (a3)</span>
