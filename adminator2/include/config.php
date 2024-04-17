45e6d0ae5fc4248ef981642a3bb87c009c238c6a
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-17 15:33:43 +0200

    f

--- adminator2/include/config.php
+++ adminator2/include/config.php
@@ -3,6 +3,15 @@
 // autoload
 require __DIR__ . '/../vendor/autoload.php';
 
+$loader = new Nette\Loaders\RobotLoader;
+
+// $loader->addDirectory(__DIR__ . '/../models');
+// $loader->addDirectory(__DIR__ . '/../app/Core');
+// $loader->addDirectory(__DIR__ . '/../app/Controllers');
+
+$loader->setTempDirectory(__DIR__ . '/../temp');
+$loader->register();
+
 // DBs inits
 
 $conn_mysql = init_mysql("Adminator2");
