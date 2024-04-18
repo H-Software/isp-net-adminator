12f48c334746e65277c2bfa1da7959a97e79ee90
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-18 15:15:17 +0200

    f

--- adminator2/vlastnici2-fs-erase.php
+++ adminator2/vlastnici2-fs-erase.php
@@ -3,6 +3,8 @@
 require("include/main.function.shared.php");
 require("include/config.php"); 
 // require_once ("include/class.php"); 
+require("include/check_login.php");
+require("include/check_level.php");
 
 if ( !( check_level($level,141) ) )
 {
