2c1a753c8007d6ce5cf444be6bdca1702313049a
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-20 20:47:46 +0200

    clean-up archiv-zmen

--- adminator2/archiv-zmen-subcat.php
+++
@@ -1,7 +0,0 @@
-<span style="margin-left: 40px; "><a href="home.php" class="odkaz-uroven-vys" >| O úrověn výš |</a></span>
-
-<span style="padding-left: 30px; "><!--<a href="archiv-zmen.php">-->Archiv změn (a3)<!--</a>--></span>
-
-<span style="padding-left: 30px; "><!--<a href="archiv-zmen-work.php">-->Archiv - Work (restartování) (a3)<!--</a>--></span>
-
-<span style="padding-left: 30px; ">Změny pro účetní (a3)</span>
