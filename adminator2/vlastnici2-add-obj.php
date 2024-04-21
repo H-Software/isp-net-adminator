c18eb9618f7138df1ed51d633880800ee626e3e4
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-22 00:26:22 +0200

    d

--- adminator2/vlastnici2-add-obj.php
+++ adminator2/vlastnici2-add-obj.php
@@ -73,7 +73,7 @@ include ("include/charset.php");
      
      if ( $res == 1){ $vysledek_write="1"; }
       
-     $add=mysql_query("INSERT INTO archiv_zmen (akce,provedeno_kym,vysledek) VALUES ('$pole3','$nick','$vysledek_write')");
+     $add=$conn_mysql->query("INSERT INTO archiv_zmen (akce,provedeno_kym,vysledek) VALUES ('$pole3','$nick','$vysledek_write')");
       
   }
   else
