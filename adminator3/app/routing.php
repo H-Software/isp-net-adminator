4c4018c7c0006ff1d7c7269213b212f0c56b2c7f
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-24 09:04:00 +0200

    p

--- adminator3/app/routing.php
+++ adminator3/app/routing.php
@@ -36,11 +36,6 @@ $app->group('', function () use ($app) {
 
     $app->get('/auth/password/change', PasswordController::class . ':getChangePassword')->setName('auth.password.change');
 	$app->post('/auth/password/change', PasswordController::class . ':postChangePassword');
-
-	// $this->map(['GET', 'POST'], '/auth/password/change', function ($req, $res, $args) use ($app) {
-    //     // 'PasswordController:getChangePassword'
-    //     return $app->getContainer()['view']->render($res, 'auth\password\change.twig');
-    // })->setName('auth.password.change');
 });
 
 $app->group('', function () {
