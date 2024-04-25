135f35a10247774fa985a98ae9708a38c44cd3d7
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-25 18:36:27 +0200

    clean-up sign-out

--- adminator3/app/Controllers/Auth/AuthController.php
+++ adminator3/app/Controllers/Auth/AuthController.php
@@ -61,75 +61,19 @@ class AuthController extends Controller
 
 	public function signout($request, $response, array $args)
 	{
-        $this->logger->info("route /logout called");
-        $this->logger->info("route /logout: dump auth->hasIdentity: ".var_export($this->container->auth->hasIdentity(), true));
-        $this->logger->info("route /logout: before: dump auth->getStorage()->isEmpty(): ".var_export($this->container->auth->getStorage()->isEmpty(), true));
+        $this->logger->info("route/logout called");
+        $this->logger->info("route/logout: dump auth->hasIdentity: ".var_export($this->container->auth->hasIdentity(), true));
+        $this->logger->info("route/logout: before: dump auth->getStorage()->isEmpty(): ".var_export($this->container->auth->getStorage()->isEmpty(), true));
     
         if ($this->container->auth->hasIdentity()) {
             $this->container->auth->clearIdentity();
         }
     
-        $this->logger->info("route /logout: dump auth->getStorage()->isEmpty(): ".var_export($this->container->auth->getStorage()->isEmpty(), true));
+        $this->logger->info("route/logout: dump auth->getStorage()->isEmpty(): ".var_export($this->container->auth->getStorage()->isEmpty(), true));
     
         //redirect:
         $url = $this->container->router->pathFor('home');
         return $response->withStatus(302)->withHeader('Location', $url);
 	}
 
-	// public function getSignOut($request, $response)
-	// {
-	// 	$this->auth->logout();
-	// 	return $response->withRedirect($this->router->pathFor('home'));
-	// }
-
-	// public function getSignIn($request, $response)
-	// {
-	// 	return $this->view->render($response, 'auth/signin.twig');
-	// }
-
-	// public function postSignIn($request, $response)
-	// {
-	// 	$auth = $this->auth->attempt(
-	// 		$request->getParam('email'),
-	// 		$request->getParam('password')
-	// 	);
-
-	// 	if (! $auth) {
-	// 		$this->flash->addMessage('error', 'Could not sign you in with those details');
-	// 		return $response->withRedirect($this->router->pathFor('auth.signin'));
-	// 	}
-
-	// 	return $response->withRedirect($this->router->pathFor('home'));
-	// }
-
-	// public function getSignUp($request, $response)
-	// {
-	// 	return $this->view->render($response, 'auth/signup.twig');
-	// }
-
-	// public function postSignUp($request, $response)
-	// {
-
-	// 	$validation = $this->validator->validate($request, [
-	// 		'email' => v::noWhitespace()->notEmpty()->email()->emailAvailable(),
-	// 		'name' => v::noWhitespace()->notEmpty()->alpha(),
-	// 		'password' => v::noWhitespace()->notEmpty(),
-	// 	]);
-
-	// 	if ($validation->failed()) {
-	// 		return $response->withRedirect($this->router->pathFor('auth.signup'));
-	// 	}
-
-	// 	$user = User::create([
-	// 		'email' => $request->getParam('email'),
-	// 		'name' => $request->getParam('name'),
-	// 		'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
-	// 	]);
-
-	// 	$this->flash->addMessage('info', 'You have been signed up');
-
-	// 	$this->auth->attempt($user->email,$request->getParam('password'));
-
-	// 	return $response->withRedirect($this->router->pathFor('home'));
-	// }
 }
