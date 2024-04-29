4905d49107239e453ef1415358a9956e24ae9b68
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-29 15:30:02 +0200

    f

--- adminator2/mk_control/mk_qos_handler.php
+++ adminator2/mk_control/mk_qos_handler.php
@@ -1,13 +1,15 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 //spravne
 error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR);
 
-require_once(__DIR__ . "/include/config.php");
+require_once(__DIR__ . "/../include/main.function.shared.php");
 
-require_once(__DIR__ . "/mk_control/ros_api_simelon.php");
+require_once(__DIR__ . "/../include/config.php");
+
+require_once(__DIR__ . "/../mk_control/ros_api_qos.php");
 
 //
 //input variables
--- adminator2/mk_control/mk_rh_restriction.php
+++ adminator2/mk_control/mk_rh_restriction.php
@@ -6,6 +6,8 @@ echo "mk_rh_restriction.php start \n";
 
 error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR);
 
+require_once(__DIR__ . "/../include/main.function.shared.php");
+
 require_once(__DIR__ . "/../include/config.php");
 
 require_once(__DIR__ . "/../mk_control/ros_api_restriction.php");
