e31c16fe226f426e2b73b1b7fc67bbf2397fef14
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-04-29 16:14:21 +0200

    d

--- adminator2/mk_control/mk_qos_handler.php
+++ adminator2/mk_control/mk_qos_handler.php
@@ -1,19 +1,16 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once(__DIR__ . "/../include/main.function.shared.php");
+require_once(__DIR__ . "/../include/config.php");
+require_once(__DIR__ . "/../mk_control/ros_api_qos.php");
+
+error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR);
+
+echo "mk_qos_handler.php started <br>\n";
 
 use RouterOS\Config;
 use RouterOS\Client;
 use RouterOS\Query;
-
-//spravne
-error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR);
-
-require_once(__DIR__ . "/../include/main.function.shared.php");
-
-require_once(__DIR__ . "/../include/config.php");
-
-require_once(__DIR__ . "/../mk_control/ros_api_qos.php");
 
 //
 //input variables
--- adminator2/mk_control/mk_rh_restriction.php
+++ adminator2/mk_control/mk_rh_restriction.php
@@ -1,20 +1,16 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once(__DIR__ . "/../include/main.function.shared.php");
+require_once(__DIR__ . "/../include/config.php");
+require_once(__DIR__ . "/../mk_control/ros_api_restriction.php");
 
-echo "mk_rh_restriction.php start \n";
+error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR);
+
+echo "mk_rh_restriction.php started <br>\n";
 
 use RouterOS\Config;
 use RouterOS\Client;
 use RouterOS\Query;
-
-error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR);
-
-require_once(__DIR__ . "/../include/main.function.shared.php");
-
-require_once(__DIR__ . "/../include/config.php");
-
-require_once(__DIR__ . "/../mk_control/ros_api_restriction.php");
 
 //
 //input variables
