4d039712678179cfab0d0b7301ffd4d23e2abe37
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-02 17:05:47 +0200

    f

--- adminator2/opravy-vypis-inc.php
+++ adminator2/opravy-vypis-inc.php
@@ -94,9 +94,9 @@ else
    
    if( $zobrazeno_limit >= $limit)
    {
-   // prozatimni reseni limitu
-   
-   $exit ="ano";
+    // prozatimni reseni limitu
+    
+    $exit ="ano";
    
    }
    
@@ -111,11 +111,14 @@ else
    $dotaz_radku_S1=$dotaz_S1->num_rows;
    
    // zde zjistit jestli uz se zobrazilo
-   for ($p = 0; $p < count($zobrazene_polozky); ++$p)
-   {
-     if( $zobrazene_polozky[$p] == $id_opravy){ $zobrazeno = "ano"; }
-     else{ $zobrazeno = "ne"; }
-   }
+    if(is_array($zobrazene_polozky))
+    {
+      for ($p = 0; $p < count($zobrazene_polozky); ++$p)
+      {
+        if( $zobrazene_polozky[$p] == $id_opravy){ $zobrazeno = "ano"; }
+        else{ $zobrazeno = "ne"; }
+      }
+    }
    
    if( $v_reseni_filtr == 1 )
    {  	    
