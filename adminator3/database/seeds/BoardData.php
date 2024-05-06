536c36ca22843f26565b7f9cc0153096915f3845
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-06 11:14:26 +0200

    f

--- adminator3/database/seeds/BoardData.php
+++ adminator3/database/seeds/BoardData.php
@@ -17,21 +17,23 @@ class BoardData extends AbstractSeed
     {
         $faker = Faker\Factory::create();
         $data = [];
+
+        // generate "fresh" posts
         for ($i = 0; $i < 4; $i++) {
             $data[] = [
                 'author'        => $faker->userName,
                 'email'         => $faker->email,
                 'subject'       => $faker->words(5, true),
-                'body'          => $faker->text(120),
-                'from_date'     => $faker->date('2024-04-08'),
-                'to_date'       => $faker->date('2024-04-10'),
+                'body'          => $faker->text(60),
+                'from_date'     => date('Y-m-d', strtotime('today')),
+                'to_date'       => date('Y-m-d', strtotime('+'. $i .' weeks')),
             ];
         }
 
+        // generate "old" posts
+        // TODO: add second loop with dates in past
+        
         // This is a cool short-hand method
         $this->insert('board', $data);
-        // $board = $this->table('board');
-        // $board->insert($data)
-        //       ->saveData();
     }
 }
