5ca059a210bf31900519075208fcfb6ecec4b741
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-07 01:17:03 +0200

    sql

--- adminator3/database/migrations/20240506234600_CreateFakturyNeuhrazeneTable.php
+++ adminator3/database/migrations/20240506234600_CreateFakturyNeuhrazeneTable.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 use App\Migration\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-// https://github.com/cartalyst/sentinel/blob/7.x/src/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
-
 class CreateFakturyNeuhrazeneTable extends Migration
 {
     public function up()
@@ -19,6 +17,7 @@ class CreateFakturyNeuhrazeneTable extends Migration
             $table->integer('par_id_vlastnika')->unsigned()->default(0);
             $table->integer('ignorovat')->unsigned()->default(0);
             $table->string('po_splatnosti_vlastnik', 50);
+            // TODO: add the rest of columns
         });
     }
 
