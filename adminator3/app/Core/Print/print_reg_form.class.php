8c90da4c3735a47bfb8a7373f3a1d85c2a6d59d8
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-10 03:37:28 +0200

    dd

--- adminator3/app/Core/Print/print_reg_form.class.php
+++ adminator3/app/Core/Print/print_reg_form.class.php
@@ -18,21 +18,6 @@ class printRegForm
 
     public $form_ec;
 
-    // stolen from https://www.php.net/manual/en/function.each.php#126076
-    public function legacy_each($array)
-    {
-        $key = key($array);
-        $value = current($array);
-        $each = is_null($key) ? false : [
-            1        => $value,
-            'value'    => $value,
-            0        => $key,
-            'key'    => $key,
-        ];
-        next($array);
-        return $each;
-    }
-
     //
     // load_input_vars
     //
@@ -40,19 +25,13 @@ class printRegForm
     {
         reset($_POST);
 
-        while (list($name, $value) = $this->legacy_each($_POST)) {
-
-            if(preg_match("/^input_/", $name) == 1) {
-
+        foreach ($_POST as $name => $value) {
+            if(preg_match("/^input\_/", $name) == 1) {
                 $this->$name = htmlspecialchars($value);
-
                 //zde pripadne doplnovani pomlcek
-
-            } //end of if(preg_math(...
-
-        } //end of while
-
-    } //end of function "load_input_vars"
+            }
+        }
+    }
 
     //
     // generate_pdf_file
