b8dab3150a5c17e79ef34e26fe9a6d0eea19aee4
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-24 21:07:48 +0200

    f

--- adminator3/app/Controllers/aboutController.php
+++ adminator3/app/Controllers/aboutController.php
@@ -10,6 +10,9 @@ class aboutController extends adminatorController
 {
     public $conn_mysql;
     public $smarty;
+    public $logger;
+    
+    protected $sentinel;
 
     private $adminator;
 
@@ -19,6 +22,8 @@ class aboutController extends adminatorController
         $this->conn_mysql = $this->container->get('connMysql');
         $this->smarty = $this->container->get('smarty');
         $this->logger = $this->container->get('logger');
+        $this->sentinel = $this->container->get('sentinel');
+
         $this->logger->info("aboutController\__construct called");
 
         $this->adminator = new \App\Core\adminator($this->conn_mysql, $this->smarty, $this->logger);
