4fb2204ef4cdb78683c10bf66ef4b3e6f3453ca3
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-24 18:39:25 +0200

    d

--- adminator3/app/Middleware/RedirectIfNotAuthenticated.php
+++ adminator3/app/Middleware/RedirectIfNotAuthenticated.php
@@ -95,7 +95,7 @@ class RedirectIfNotAuthenticated
 
             // from https://github.com/cartalyst/sentinel/issues/519#issuecomment-559742227
             $currentLoggedInUser = $this->sentinel->getUser();
-            $persistence = $this->sentinel->getPersistenceRepository();
+            // $persistence = $this->sentinel->getPersistenceRepository();
             // $persistence->flush();
 
             // $this->sentinel->getPersistenceRepository()->flush();
