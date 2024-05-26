95bb1efe7479c654bcb2de4df80c70d0cc060b5e
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-26 16:16:40 +0200

    ff

--- adminator3/app/Controllers/adminatorController.php
+++ adminator3/app/Controllers/adminatorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 // use Psr\Http\Message\ResponseFactoryInterface;
