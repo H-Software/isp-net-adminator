6a4c1b0133b5ec9256dd25b40bcd3b6d68a0b296
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-27 19:05:49 +0200

    ff

--- adminator3/scripts/work_diff_auto.php
+++ adminator3/scripts/work_diff_auto.php
@@ -24,7 +24,7 @@ $conn_mysql = init_mysql("Adminator2");
 
 // require __DIR__ ."/../boostrap/dependencies.php";
 
-// end of bootstrap 
+// end of bootstrap
 
 $html_tags = 1;
 
@@ -32,76 +32,34 @@ $hlaska =  "work-diff-auto.php start [".strftime("%d/%m/%Y %H:%M:%S", time())."]
 echo $hlaska;
 
 $output_main .= $hlaska;
-       
-    $rs = $conn_mysql->query("SELECT id, number_request FROM workitems ORDER BY id ");
-    $num_rows = $rs->num_rows;
-     
-    if ($num_rows == 0 ) 
-    { 
-        echo " INFO: no requests on the system \n"; 
-        $output_main .= " INFO: no requests on the system \n";
-    }
-    else
-    {
-      while($data = $rs->fetch_array() )
-      {
+
+$rs = $conn_mysql->query("SELECT id, number_request FROM workitems ORDER BY id ");
+$num_rows = $rs->num_rows;
+
+if ($num_rows == 0) {
+    echo " INFO: no requests on the system \n";
+    $output_main .= " INFO: no requests on the system \n";
+} else {
+    while($data = $rs->fetch_array()) {
         $id = $data["id"];
         $number_request = $data["number_request"];
 
         execute_action($number_request, $id);
-	
-      } //end of while
-	
-   } // end of else if num_rows == 0
+
+    } //end of while
+
+} // end of else if num_rows == 0
 
 
 echo "work-diff.php stop [".strftime("%d/%m/%Y %H:%M:%S", time())."] \n";
 $output_main .= "work-diff.php stop [".strftime("%d/%m/%Y %H:%M:%S", time())."] \n";
 
-if( preg_match("/.*<span.*>.*/", $output_main) )
-{ $soubor = fopen(__DIR__ . "/../logs/reinhard.remote.log", "w"); }
-else
-{ 
+if(preg_match("/.*<span.*>.*/", $output_main)) {
+    $soubor = fopen(__DIR__ . "/../logs/reinhard.remote.log", "w");
+} else {
     $output_main = "- - - - - - - - - - - - - -\n".$output_main;
     $soubor = fopen(__DIR__ . "/../logs/reinhard.remote.log", "a");
 }
- 
-fwrite($soubor, $output_main); 
+
+fwrite($soubor, $output_main);
 fclose($soubor);
-
-//
-// zde uz jen funkce
-//
-
-function execute_request($cmd, $mess_ok, $mess_er) 
-{
-    global $html_tags, $output_main;
-    
-    exec($cmd, $output, $rs);
-    //system($cmd, $rs);
-
-    //print_r($output);
-    $output_main .= "\n".implode("\n ", $output)."\n";
-    	    
-    if($rs == "0")
-    { 		
-        if($html_tags == 1)
-        { $hlaska = "  <span class=\"work-ok\">".$mess_ok." (message: ".$rs.")</span>\n"; }
-        else
-        { $hlaska = "  ".$mess_ok." (message: ".$rs.")\n"; }
-
-        echo $hlaska;
-        $output_main .= $hlaska;
-    }
-    else
-    { 
-        if($html_tags == 1)
-        { $hlaska = "  <span class=\"work-error\">".$mess_er." (message: ".$rs.")</span>\n"; }
-        else
-        { $hlaska = "  ".$mess_er." (message: ".$rs.")\n"; }
-
-        echo $hlaska; 
-        $output_main .= $hlaska;
-    }
-
-} //end of function execute_action
