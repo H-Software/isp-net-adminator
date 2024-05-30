cc5c17c85a433d1268af9a72b9f7c2d3118b98d7
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-30 18:43:44 +0200

    f

--- adminator3/app/Renderer/Renderer.php
+++ adminator3/app/Renderer/Renderer.php
@@ -70,6 +70,8 @@ final class Renderer
         try {
             $content = $this->smarty->fetch($template);
         } catch (\SmartyException $e) {
+            // try render "known" template
+            
             $this->logger->error(__CLASS__ . "\\" . __FUNCTION__ . ": caught SmartyException: " . var_export($e->getMessage(), true));
 
             $this->smarty->assign("page_title", "Renderer Error");
