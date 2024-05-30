7e84407f7cf6b266cb8c901ba270a908e5d6328f
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-30 16:40:10 +0200

    slimification partner-servis - part2 (#220)

--- adminator3/app/Core/shared/partner.servis.class.php
+++ adminator3/app/Core/shared/partner.servis.class.php
@@ -184,7 +184,6 @@ class partner_servis
         }
 
         while($array = pg_fetch_array($rs_vlastnici)) {
-
             $row = array();
 
             $row["id_cloveka"] = $array["id_cloveka"];
@@ -194,7 +193,6 @@ class partner_servis
             $row["mesto"] = $array["mesto"];
 
             $RetArray[] = $row;
-
         }
 
         return $RetArray;
@@ -644,13 +642,11 @@ class partner_servis
                 $output .= "<td class=\"".$class."\" >&nbsp;</td>";
 
                 if($this->vyrizeni == true) {
-                    $output .= "<td colspan=\"2\" class=\"".$class."\"><a href=\"".$_SERVER["PHP_SELF"]."?accept=1&id=".intval($data["id"])."\">akceptovat</a></td>";
+                    $output .= "<td colspan=\"2\" class=\"".$class."\"><a href=\"?accept=1&id=".intval($data["id"])."\">akceptovat</a></td>";
                 } elseif($this->update == true) {
-                    $output .= "<td colspan=\"2\" class=\"".$class."\"><a href=\"".$_SERVER["PHP_SELF"]."?edit=1&id=".intval($data["id"])."\">upravit</a></td>";
+                    $output .= "<td colspan=\"2\" class=\"".$class."\"><a href=\"?edit=1&id=".intval($data["id"])."\">upravit</a></td>";
                 } else {
-
                     $output .= "<td class=\"".$class."\" ><span style=\"font-size: 13px; \">&nbsp;</span></td>";
-
                 }
 
                 $output .= "</tr>";
