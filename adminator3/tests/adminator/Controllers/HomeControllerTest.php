6c6043fd0574ae32b4c68de3f124859eacba75a1
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-31 17:42:59 +0200

    home post

--- adminator3/tests/adminator/Controllers/HomeControllerTest.php
+++ adminator3/tests/adminator/Controllers/HomeControllerTest.php
@@ -134,4 +134,66 @@ final class HomeControllerTest extends AdminatorTestCase
         // non-common negative asserts
         $this->assertStringNotContainsStringIgnoringCase("nepodařil", $responseContent, " found word, which indicates error(s) or failure(s)");
     }
+
+    public function test_ctl_home_w_mocked_auth_and_opravy_post()
+    {
+        // $this->markTestSkipped('under construction');
+        $self = $this;
+
+        $request = Request::create(
+            '/home',
+            'POST',
+            [],
+            [],
+            [],
+            [
+                'SCRIPT_URL' => "/home",
+            ],
+        );
+
+        $request->overrideGlobals();
+        $serverRequest = self::$psrHttpFactory->createRequest($request);
+
+        $container = self::initDIcontainer(true, false);
+
+        $adminatorMock = self::initAdminatorMockClass($container);
+        $this->assertIsObject($adminatorMock);
+
+        $opravyMock = m::mock(
+            \opravy::class,
+        );
+        $opravyMock->shouldReceive('vypis_opravy')->andReturn(["mock -> no data"]);
+
+        $response = self::callControllerFunction(
+            $serverRequest,
+            'App\Controllers\HomeController',
+            'home',
+            $container,
+            array(
+                "adminatorMock" => $adminatorMock,
+                "opravyMock" => $opravyMock,
+            )
+        );
+        $responseContent = $response->getBody()->__toString();
+
+        AdminatorAssert::assertHomePagePanels($response, $responseContent);
+
+        // page specific asserts
+        //
+        // self::assertXpathQueryContentRegex($response, '/html/head/title', '/^Bulletin.*Board.*/');
+
+        $assertKeywordsHome = array(
+            '<title>Adminator3 :: úvodní stránka</title>',  // corrent title
+        );
+        //
+
+        foreach ($assertKeywordsHome as $w) {
+            $this->assertStringContainsString($w, $responseContent, "missing string \"" . $w . "\" in response body");
+        }
+
+        // negative assert
+        // check word: nelze
+        $this->assertStringNotContainsStringIgnoringCase("chyba", $responseContent, "found word (" . $w. "), which indicates error(s) or failure(s)");
+        $this->assertStringNotContainsStringIgnoringCase("nepodařil", $responseContent, "found word (" . $w. "), which indicates error(s) or failure(s)");
+    }
 }
