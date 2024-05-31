816a6bf1163c5bc7760eaf7325afc24e84e6eb14
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-31 19:36:30 +0200

    slimification topology/node-add

---
+++ adminator3/app/Core/Topology/nodeAction.class.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Core\Topology;
+
+use App\Core\adminator;
+use Psr\Container\ContainerInterface;
+use Exception;
+
+class RouterAction extends adminator
+{
+    public \mysqli|\PDO $conn_mysql;
+
+    // public \Smarty $smarty;
+
+    public \Monolog\Logger $logger;
+
+    protected $settings;
+
+    protected $sentinel;
+
+    protected $work;
+
+    protected $loggedUserEmail;
+
+    public $csrf_html;
+
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->conn_mysql = $container->get('connMysql');
+        // $this->conn_pgsql = $container->get('connPgsql');
+        $this->logger = $container->get('logger');
+        $this->sentinel = $container->get('sentinel');
+
+        $this->logger->info(__CLASS__ . "\\" . __FUNCTION__ . " called");
+
+        $this->loggedUserEmail = $this->sentinel->getUser()->email;
+
+        // $this->work = new \App\Core\work($container);
+    }
+
+    public function add()
+    {
+        $this->logger->info(__CLASS__ . "\\" . __FUNCTION__ . " called");
+
+    }
+
+    public function update()
+    {
+        $this->logger->info(__CLASS__ . "\\" . __FUNCTION__ . " called");
+
+    }
+}
