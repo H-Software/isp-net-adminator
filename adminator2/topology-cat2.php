714e8e69721a35be163f27b73fcc692d9534c7a7
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-31 23:01:28 +0200

    adminator2: fix rendering, remove dead links (#235)

--- adminator2/topology-cat2.php
+++ adminator2/topology-cat2.php
@@ -1,7 +1,7 @@
 
-<span style="padding-left: 30px; "><!--<a href="topology-router-list.php">-->Routery<!--</a>--> (<a href="topology-router-add.php" >přidání</a>)</span>
+<span style="padding-left: 30px; ">Routery (přidání)</span>
 
-<span style="padding-left: 30px; "><!--<a href="topology-nod-list.php">-->Výpis lokalit/nodů<!--</a>--></span>
-<span style="padding-left: 30px; "><a href="topology-nod-add.php">Přidání lokality/nodu</a></span>
+<span style="padding-left: 30px; ">Výpis lokalit/nodů</span>
+<span style="padding-left: 30px; ">Přidání lokality/nodu</span>
 
 <span style="padding-left: 30px; "><a href="topology-user-list.php">Výpis objektů dle přiřazení / dle nodů</a></span>
