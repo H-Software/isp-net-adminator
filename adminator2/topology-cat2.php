2f184ecfae1ba68457f7410b0ddd2b5de2de8f46
Patrik Majer <patrik.majer.pisek@gmail.com>
2024-05-31 22:56:26 +0200

    f

--- adminator2/topology-cat2.php
+++ adminator2/topology-cat2.php
@@ -1,5 +1,5 @@
 
-<span style="padding-left: 30px; "><!--<a href="topology-router-list.php">-->Routery<!--</a>--> (<a href="topology-router-add.php" >přidání</a>)</span>
+<span style="padding-left: 30px; ">Routery (přidání)</span>
 
 <span style="padding-left: 30px; ">Výpis lokalit/nodů</span>
 <span style="padding-left: 30px; ">Přidání lokality/nodu</span>
