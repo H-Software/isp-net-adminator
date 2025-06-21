5ed5d17a5037b00a14ec9fcde3d40f6d137c0cc9
Patrik Majer <patrik.majer.pisek@gmail.com>
2025-06-21 17:20:19 +0200

    add basic redis & async tests

--- adminator3/tests/asynqClient/ClientTest.php
+++ adminator3/tests/asynqClient/ClientTest.php
@@ -2,42 +2,70 @@
 
 namespace HyssaDev\HibikenAsynqClient\Tests;
 
-use Ramsey\Uuid\Uuid;
+// use Ramsey\Uuid\Uuid;
 use HyssaDev\HibikenAsynqClient\Client;
 use Illuminate\Support\Facades\Redis;
-use Illuminate\Container\Container;
+// use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Facade;
+use Illuminate\Redis\Connections\PhpRedisConnection;
+// use Lunaweb\RedisMock\MockPredisConnection;
+use Lunaweb\RedisMock\Providers\RedisMockServiceProvider;
 
-// TODO: enable this
-// class AsynqClientTest extends \PHPUnit\Framework\TestCase
-// {
-// public function testEnqueue()
-// {
-//     $app = new Container();
-//     $app->singleton('app', 'Illuminate\Container\Container');
+class AsynqClientTest extends \PHPUnit\Framework\TestCase
+{
+    public $app;
 
-//     /**
-//     * Set $app as FacadeApplication handler
-//     */
-//     Facade::setFacadeApplication($app);
+    public function getEnvironmentSetUp($app)
+    {
 
-//     $redis = new Redis($app, 'phpredis', [
-//         'cluster' => false,
-//         'default' => [
-//             'host'     => '127.0.0.1',
-//             'port'     => 6379,
-//             'database' => 0,
-//         ],
-//     ]);
+        $app['config']->set('app.debug', true);
+        $app['config']->set('database.redis.client', 'mock');
 
-//     $clinet = new Client($redis);
-//     $res = $clinet->Enqueue([
-//         'typename' => 'newtest:user:xxxx',
-//         'payload' => [
-//             'test' => 'xxxx',
-//             'user' => 1111
-//         ]
-//     ]);
-//     $this->assertTrue($res);
-// }
-// }
+
+        $app->register(RedisMockServiceProvider::class);
+
+        $this->app = $app;
+    }
+
+    public function testRedisConnectionInstance()
+    {
+
+        $this->assertInstanceOf(PhpRedisConnection::class, Redis::connection());
+
+    }
+
+    public function testSetAndGet()
+    {
+
+        Redis::set('key', 'test');
+        $this->assertEquals('test', Redis::get('key'));
+
+    }
+
+    public function testEnqueue()
+    {
+        /**
+        * Set $app as FacadeApplication handler
+        */
+        Facade::setFacadeApplication($this->app);
+
+        $redis = new Redis();
+
+        /**
+        * Set $app as FacadeApplication handler
+        */
+        Facade::setFacadeApplication($this->app);
+
+        $redis = new Redis($this->app);
+
+        $clinet = new Client($redis);
+        $res = $clinet->Enqueue([
+            'typename' => 'newtest:user:xxxx',
+            'payload' => [
+                'test' => 'xxxx',
+                'user' => 1111
+            ]
+        ]);
+        $this->assertTrue($res);
+    }
+}
